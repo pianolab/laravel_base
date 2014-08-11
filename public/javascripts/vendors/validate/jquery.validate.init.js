f8753b2935ebab2a89f01820e9d3c5129d5c7e94
Walmir Neto <walmir@pianolab.com.br>
2014-08-11 20:30:52 -0300

    add jquery validate

--- public/javascripts/vendors/validate/jquery.validate.init.js
+++ public/javascripts/vendors/validate/jquery.validate.init.js
@@ -1,45 +1,51 @@
 $.validator.setDefaults({
   errorElement: 'span',
+  errorClass: "error",
+  validClass: "valid",
+
+  onkeyup: false,
+  // onclick: true,
 
   ignore: [],
 
-  highlight: function(element) {
-    $(element).closest('.control-group')
-      .removeClass('success').addClass('error');
+  errorPlacement: function(error, element) {
+    var icon = $('<i>', { 'class': 'fa fa-times' });
+    error.attr('data-placement', 'left').attr('data-content', error.text());
+    error.insertAfter(element).text('').html(icon).popover({ trigger: 'hover' }).popover('show');
+
+    setTimeout( function() { error.popover('hide'); }, 2000);
   },
-  
+
+  highlight: function(element, errorClass, validClass) {
+    $(element).removeClass(validClass).addClass(errorClass)
+      .closest('.control-group,.form-group')
+      .removeClass('has-success').addClass('has-error');
+  },
+
+  unhighlight: function(element, errorClass, validClass) {
+    $(element).removeClass(errorClass).addClass(validClass)
+      .closest('.control-group,.form-group')
+      .removeClass('has-error').addClass('has-success');
+  },
+
   success: function(element) {
-    var icon;
-    icon = $('<i>').addClass('icon-ok').text('ok');
-    element.html(icon).addClass('valid').closest('.control-group')
-      .removeClass('error').addClass('success');
-  }
+    $(element).addClass('valid').attr('data-content', '').html('<i class="fa fa-check" />').popover('destroy');
+  },
 });
 
 $.validator.addMethod(
   'dateBR', function(value, element) {
-    return value.match(/^([0-2]\d|[3][0-1])\/(0\d|1[0-2])\/([1-2]\d{3})$/);
+    return value.match(/^(0[1-9]|[12][0-9]|3[01])[- \/.](0[1-9]|1[012])[- \/.](19|20)\d\d$/);
   },
-  "Please enter a date in the format dd/mm/yyyy."
+  "Invalid date. Eg: dd/mm/yyyy."
 );
 
-$.extend($.validator.messages, {
-  required: "Este campo é obrigatório.",
-  remote: "Por favor, corrija este campo.",
-  email: "Por favor, digite um email válido.",
-  url: "Por favor, digite uma URL válida.",
-  date: "Por favor, digite uma data válida (mm/dd/yyyy)",
-  dateISO: "Por favor, digite uma data válida (yyyy-mm-dd)",
-  dateISO: "Por favor, digite uma data válida (dd/mm/yyyy)",
-  number: "Por favor, digite um número válida.",
-  digits: "Por favor, digite somente dígitos.",
-  creditcard: "Por favor, digite um cartão de crédito válido.",
-  equalTo: "Por favor, digite o mesmo valor novamente.",
-  accept: "Por favor, digite um valor com uma extensão válida.",
-  maxlength: $.validator.format("Por favor, digite não mais que {0} caracteres."),
-  minlength: $.validator.format("Por favor, digite ao menos {0} caracteres."),
-  rangelength: $.validator.format("Por favor, digite um valor entre {0} e {1} caracteres de comprimento."),
-  range: $.validator.format("Por favor, digite um valor entre {0} e {1}."),
-  max: $.validator.format("Por favor, digite um valor menor ou igual a {0}."),
-  min: $.validator.format("Por favor, digite um valor maior ou igual a {0}.")
+$(document).on('ready page:load', function () {
+  $('.form-group').on('click', '.popover', function () {
+    $(this).fadeOut();
+  });
+
+  var input_group = $('.input-group').find('.error').siblings('.input-group-btn');
+  var input_group_width = input_group.width() + parseInt(input_group.siblings('.error').css('right'));
+  input_group.siblings('.error').css({ 'right': input_group_width + 'px' });
 });
