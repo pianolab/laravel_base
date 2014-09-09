20169ab1ec82df6c23e4b6510aea5f6e1cc46de7
Walmir Neto <walmir@pianolab.com.br>
2014-09-09 20:01:55 -0300

    adjustments

--- public/javascripts/modules/posts.js
+++ public/javascripts/modules/posts.js
@@ -2,12 +2,10 @@ var Post = {
 
     init: function () {
         this.validations();
-        this.uploadify();
-        this.setLabel();
-        this.main();
+        this.uploadifive();
     },
 
-    uploadify: function () {
+    uploadifive: function () {
         Uploadifive.many('#attachment_post');
     },
 
@@ -25,46 +23,6 @@ var Post = {
                     required: true
                 }
             }
-        });
-    },
-
-    setLabel: function () {
-        $(Uploadifive.wrap).on('blur', '.attachment-label', function () {
-            var element = $(this);
-
-            Uploadifive.update( element.closest('.fields').data('id'), {
-                label: element.val()
-            })
-            .success( function (response) {
-                if (response.success) {};
-            });
-        });
-    },
-
-    main: function () {
-        $(Uploadifive.wrap).on('click', '.attachment-main', function () {
-            var element = $(this);
-            var current_value = element.attr('data-current-value');
-
-            Uploadifive.update( element.closest('.actions').data('id'), {
-                is_main: current_value == 1 ? 0 : 1
-            })
-            .success( function (response) {
-                if (response.success) {
-                    element.attr('data-current-value', response.attachment_is_main);
-
-                    if (response.attachment_is_main == 1) {
-                        element.find('i')
-                            .removeClass('fa-times').removeClass('text-danger')
-                            .addClass('fa-check').addClass('text-success');
-                    }
-                    else {
-                        element.find('i')
-                            .removeClass('fa-check').removeClass('text-success')
-                            .addClass('fa-times').addClass('text-danger');
-                    };
-                };
-            });
         });
     },
 };
--- public/javascripts/vendors/uploadifive/jquery.uploadifive.init.js
+++ public/javascripts/vendors/uploadifive/jquery.uploadifive.init.js
@@ -6,6 +6,8 @@ var Uploadifive = {
     init: function () {
         this.destroy();
         this.show_actions();
+        this.set_label();
+        this.main();
     },
 
     show_actions: function () {
@@ -121,7 +123,47 @@ var Uploadifive = {
                 });
             };
         });
-    }
+    },
+
+    set_label: function () {
+        $(Uploadifive.wrap).on('blur', '.attachment-label', function () {
+            var element = $(this);
+
+            Uploadifive.update( element.closest('.fields').data('id'), {
+                label: element.val()
+            })
+            .success( function (response) {
+                if (response.success) {};
+            });
+        });
+    },
+
+    main: function () {
+        $(Uploadifive.wrap).on('click', '.attachment-main', function () {
+            var element = $(this);
+            var current_value = element.attr('data-current-value');
+
+            Uploadifive.update( element.closest('.actions').data('id'), {
+                is_main: current_value == 1 ? 0 : 1
+            })
+            .success( function (response) {
+                if (response.success) {
+                    element.attr('data-current-value', response.attachment_is_main);
+
+                    if (response.attachment_is_main == 1) {
+                        element.find('i')
+                            .removeClass('fa-times').removeClass('text-danger')
+                            .addClass('fa-check').addClass('text-success');
+                    }
+                    else {
+                        element.find('i')
+                            .removeClass('fa-check').removeClass('text-success')
+                            .addClass('fa-times').addClass('text-danger');
+                    };
+                };
+            });
+        });
+    },
 };
 
 $(document).on("ready", function () { Uploadifive.init(); });
